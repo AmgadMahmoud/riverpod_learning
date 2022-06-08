6ecb3febc638067822b491c8901cfb2cf73e7eae
Amgad Mahmoud <amgad.mah2@gmail.com>
2022-06-08 08:22:10 +0200

    trying different provider types

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final counterProvider = StateProvider((ref) => 0);
+// Fake WebSocket to simulate handling stream of data in the provider
+abstract class WebSocketClient {
+  Stream<int> getCounterStream([int start]);
+}
+
+class FakeWebSocketClient implements WebSocketClient {
+  @override
+  Stream<int> getCounterStream([int start = 0]) async* {
+    // we added optional parameter to start from specific number
+    int i = start;
+    while (true) {
+      await Future.delayed(const Duration(milliseconds: 500));
+      yield i++;
+    }
+  }
+}
+
+// provider to access webSocket to get data from there
+final webSocketClientProvider = Provider<WebSocketClient>((ref) {
+  return FakeWebSocketClient();
+});
+
+// default Provider is readonly
+//final counterProvider = Provider((ref) => 0);
+
+// to update the data in the provider, use StateProvider
+// final counterProvider = StateProvider((ref) => 0);
 
 // we can add autoDispose to reset counter when we go back to home page
 // final counterProvider = StateProvider.autoDispose((ref) => 0);
+
+// we use StreamProvider to handle stream of data
+// final counterProvider = StreamProvider<int>((ref) {
+//   final wsClient = ref.watch(webSocketClientProvider);
+//   return wsClient.getCounterStream();
+// });
+
+// we used family to pass an initial value for the counter
+// first int for the provider type
+// second int for the passed initial counter
+final counterProvider = StreamProvider.family<int, int>((ref , start) {
+  final wsClient = ref.watch(webSocketClientProvider);
+  return wsClient.getCounterStream(start);
+});
 
 void main() {
   runApp(
@@ -62,23 +102,69 @@ class CounterPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final int counter = ref.watch(counterProvider);
+    // we use watch to listen for every change in the provider data
+    final AsyncValue<int> counter = ref.watch(counterProvider(5));
+
+    // we use listen to listen for the changes in the provider
+    // in order to show alert dialog under certain condition
+    // we use listen<int> to listen to int value
+    // ref.listen<int>(counterProvider, (previous, next) {
+    //   if (next >= 5) {
+    //     showDialog(
+    //         context: context,
+    //         builder: (context) {
+    //           return AlertDialog(
+    //             title: const Text('Warning'),
+    //             content: const Text(
+    //                 'Counter dangerously high, Consider resetting it'),
+    //             actions: [
+    //               TextButton(
+    //                   onPressed: () {
+    //                     Navigator.of(context).pop();
+    //                   },
+    //                   child: const Text('Ok'))
+    //             ],
+    //           );
+    //         });
+    //   }
+    // });
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter'),
+        // actions: [
+        //   IconButton(
+        //     onPressed: () {
+        //       // we use invalidate to reset the value in the provider
+        //       ref.invalidate(counterProvider);
+        //     },
+        //     icon: const Icon(
+        //       Icons.refresh,
+        //     ),
+        //   ),
+        // ],
       ),
       body: Center(
         child: Text(
-          counter.toString(),
+          // counter.toString(),
+
+          // we used when to listen to the data whether it came data of error or it's loading
+          counter
+              .when(
+                  data: (int value) => value,
+                  error: (Object e, _) => e,
+                  loading: () => 5)
+              .toString(),
           style: Theme.of(context).textTheme.displayMedium,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(counterProvider.notifier).state++;
-        },
-        child: const Icon(Icons.add),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     // we use read to just get the value of the provider once just to update it
+      //     ref.read(counterProvider.notifier).state++;
+      //   },
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
 }
